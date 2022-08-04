98160a1c6147d2324bf218138f97af13be917def
Petr Hučík <petr@getdatakick.com>
2022-08-04 09:48:27 +0200

    Version 3.1.0

--- blocknewsletter.php
+++ blocknewsletter.php
@@ -92,7 +92,7 @@ class Blocknewsletter extends Module
         $this->tb_min_version = '1.0.0';
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6.99.99');
 
-        $this->version = '3.0.4';
+        $this->version = '3.1.0';
         $this->author = 'thirty bees';
         $this->error = false;
         $this->valid = false;
